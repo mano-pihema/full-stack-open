59754aa1d1bcda60d3bbb69d6dab046be47d20c9
mano-pihema <mano-pihema-laptop@LAPTOP-MNKDSN6R>
2023-09-12 13:52:37 +1200

    8.8 done

--- part8/library-front/src/App.js
+++ part8/library-front/src/App.js
@@ -2,9 +2,17 @@ import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from './components/queries'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+
+  const authors = useQuery(ALL_AUTHORS)
+
+  if (authors.loading) {
+    return <div>loading...</div>
+  }
 
   return (
     <div>
@@ -14,7 +22,7 @@ const App = () => {
         <button onClick={() => setPage('add')}>add book</button>
       </div>
 
-      <Authors show={page === 'authors'} />
+      <Authors show={page === 'authors'} persons={authors.data.allAuthors} />
 
       <Books show={page === 'books'} />
 
